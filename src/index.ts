f3b73b73c0de3951dfacb1f4e59340036e1a9b44
circle67 <swampycroc19@gmail.com>
2022-08-28 14:43:20 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { WORD_LIST } from './wordList';
 import { VALID_GUESSES } from './validGuesses';
 import { check } from './wordle';
-const alphabet = require('alphabet');
+import alphabet from 'alphabet';
 
 // Styles import
 import './styles.css';
@@ -26,7 +26,7 @@ interface Stats {
 	inThreePercent: number;
 }
 
-let game: WordleGame = {
+const game: WordleGame = {
 	row: 0,
 	col: 0,
 	secretWord: generateSecretWord(),
@@ -54,8 +54,8 @@ window.onbeforeunload = () => {
 };
 
 document.getElementById('resetButton').addEventListener('click', () => {
-	let board = document.getElementById('board');
-	let keyboard = document.getElementById('keyboard');
+	const board = document.getElementById('board');
+	const keyboard = document.getElementById('keyboard');
 
 	game.row = 0;
 	game.col = 0;
@@ -67,11 +67,11 @@ document.getElementById('resetButton').addEventListener('click', () => {
 
 	// x, y: indexes (as in "let i" for a loop that does not have another loop)
 	for (let x = 0; x < board.childNodes.length; x++) {
-		let row = board.childNodes.item(x);
+		const row = board.childNodes.item(x);
 		for (let y = 0; y < row.childNodes.length; y++) {
-			let col = row.childNodes.item(y);
+			const col = row.childNodes.item(y);
 			col.firstChild.textContent = '';
-			// @ts-expect-error
+			// @ts-expect-error className property does not exist according to typescript
 			col.className = 'input';
 		}
 	}
@@ -85,12 +85,12 @@ document.getElementById('resetButton').addEventListener('click', () => {
 
 document.getElementById('shareButton').addEventListener('click', () => {
 	if (game.end && game.win) {
-		let shareString: string = `MyWordle ${game.stateHistory.length}/${game.maxTries}\n`;
+		let shareString = `MyWordle ${game.stateHistory.length}/${game.maxTries}\n`;
 		// x, y: indexes (as in "let i" for a loop that does not have another loop)
 		for (let x = 0; x < game.stateHistory.length; x++) {
-			let state: string = game.stateHistory[x];
+			const state: string = game.stateHistory[x];
 			for (let y = 0; y < game.stateHistory[x].length; y++) {
-				let char: string = state[y];
+				const char: string = state[y];
 				if (char === '0') {
 					shareString += '🟩';
 				} else if (char === '1') {
@@ -111,9 +111,9 @@ document.getElementById('shareButton').addEventListener('click', () => {
 });
 
 document.getElementById('statsButton').addEventListener('click', () => {
-	let played: number = game.playHistory.length;
-	let winNumber: number = 0;
-	let inThreeNumber: number = 0;
+	const played: number = game.playHistory.length;
+	let winNumber = 0;
+	let inThreeNumber = 0;
 	game.playHistory.forEach((play) => {
 		if (play !== -1) {
 			winNumber++;
@@ -122,7 +122,7 @@ document.getElementById('statsButton').addEventListener('click', () => {
 			inThreeNumber++;
 		}
 	});
-	let stats: Stats = {
+	const stats: Stats = {
 		played: played,
 		winPercent: played === 0 ? 0 : Math.round((winNumber / played) * 100),
 		inThreePercent: played === 0 ? 0 : Math.round((inThreeNumber / played) * 100),
@@ -135,8 +135,8 @@ document.getElementById('statsButton').addEventListener('click', () => {
 document.getElementById('keyboard').childNodes.forEach((row) => {
 	row.childNodes.forEach((key) => {
 		key.addEventListener('click', (e) => {
-			// @ts-expect-error
-			let virtKey: string = e.target.getAttribute('key');
+			// @ts-expect-error .getAttribute will not work with typescript checking
+			const virtKey: string = e.target.getAttribute('key');
 			if (virtKey !== 'Enter' && virtKey !== 'Backspace') {
 				input(virtKey);
 			} else if (virtKey === 'Enter' && game.col === 5) {
@@ -149,7 +149,7 @@ document.getElementById('keyboard').childNodes.forEach((row) => {
 });
 
 function input(letter: string) {
-	let board = document.getElementById('board');
+	const board = document.getElementById('board');
 	if (game.col < game.wordLength) {
 		board.childNodes.item(game.row).childNodes.item(game.col).firstChild.textContent = letter;
 
@@ -159,7 +159,7 @@ function input(letter: string) {
 }
 
 function backspace() {
-	let board = document.getElementById('board');
+	const board = document.getElementById('board');
 	if (game.col > 0) {
 		board.childNodes.item(game.row).childNodes.item(game.col - 1).firstChild.textContent = '';
 
@@ -169,12 +169,9 @@ function backspace() {
 }
 
 function submit() {
-	let valid: boolean = false;
-	let state: string = ''; // 0: match full; 1: match partial; 2: match none
+	let state = ''; // 0: match full; 1: match partial; 2: match none
 
 	if (VALID_GUESSES.includes(game.currentInput)) {
-		valid = true;
-
 		state = check(game.currentInput, game.secretWord).join('');
 
 		game.stateHistory.push(state);
@@ -192,20 +189,20 @@ function submit() {
 }
 
 function flip(state: string) {
-	let board = document.getElementById('board');
-	let keyboard = document.getElementById('keyboard');
+	const board = document.getElementById('board');
+	const keyboard = document.getElementById('keyboard');
 
 	for (let i = 0; i < state.length; i++) {
 		if (state[i] === '0') {
-			// @ts-expect-error
+			// @ts-expect-error classList property does not exist according to typescript
 			board.childNodes.item(game.row).childNodes.item(i).classList.add('state-0');
 			keyboard.querySelector(`[key="${game.currentInput[i].toUpperCase()}"]`).className = 'key state-0';
 		} else if (state[i] === '1') {
-			// @ts-expect-error
+			// @ts-expect-error classList property does not exist according to typescript
 			board.childNodes.item(game.row).childNodes.item(i).classList.add('state-1');
 			keyboard.querySelector(`[key="${game.currentInput[i].toUpperCase()}"]`).className = 'key state-1';
 		} else {
-			// @ts-expect-error
+			// @ts-expect-error classList property does not exist according to typescript
 			board.childNodes.item(game.row).childNodes.item(i).classList.add('state-2');
 			keyboard.querySelector(`[key="${game.currentInput[i].toUpperCase()}"]`).className = 'key state-2';
 		}
@@ -243,18 +240,18 @@ function syncPlayHistory() {
 }
 
 function generateSecretWord(): string {
-	let random: number = Math.floor(Math.random() * WORD_LIST.length);
+	const random: number = Math.floor(Math.random() * WORD_LIST.length);
 	return WORD_LIST[random];
 }
 
 function showMessage(message: string) {
 	document.getElementById('message').firstChild.textContent = message;
-	// @ts-expect-error
+	// @ts-expect-error className property does not exist according to typescript
 	document.getElementById('message').firstChild.className = 'showing';
 	document.getElementById('message').classList.remove('hidden');
 	document.getElementById('message').classList.add('showing');
 	setTimeout(() => {
-		// @ts-expect-error
+		// @ts-expect-error className property does not exist according to typescript
 		document.getElementById('message').firstChild.className = 'hidden';
 		document.getElementById('message').classList.remove('showing');
 		document.getElementById('message').classList.add('hidden');
